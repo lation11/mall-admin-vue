682e2eb970b71166655fc28bcc6f6f4cc86aa3da
panyongjian <18846151446@163.com>
2024-04-08 11:01:26 +0800

    提交代码到github

---
+++ src/views/layout/headerBar/headerBar.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="home-header">
+        <div class="home-header-left">
+            <a-button type="primary" @click="toggleCollapsed">
+                <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+            </a-button>
+            <a-breadcrumb>
+                <a-breadcrumb-item>首页</a-breadcrumb-item>
+                <a-breadcrumb-item><a href="">操作</a></a-breadcrumb-item>
+            </a-breadcrumb>
+        </div>
+        <ul class="home-use-box">
+            <li>你好 admin<a-icon class="icon-down" type="down" /></li>
+            <li>退出</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        collapsed:{
+            type:Boolean
+        }
+    },
+    methods: {
+        toggleCollapsed() {
+            this.$emit('update:collapsed',!this.collapsed )
+        },
+    },
+}
+</script>
+
+<style lang="less" scoped>
+.home-header {
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #eee;
+    padding: 0 20px;
+
+    .home-header-left {
+        display: flex;
+        align-items: center;
+
+        .ant-btn-primary {
+            margin-right: 15px;
+        }
+
+    }
+
+    .home-use-box {
+        height: 50px;
+        line-height: 50px;
+        li {
+            padding: 0 30px;
+            text-align: center;
+            cursor: pointer;
+            .icon-down{
+                margin-left: 5px;
+            }
+            &:not(:first-child) {
+                display: none;
+                border: 1px solid #eee;
+                border-top: transparent;
+                &:hover {
+                    background-color: #eee;
+                    color: #999;
+                }
+            }
+        }
+
+        &:hover li {
+            display: block;
+        }
+
+    }
+}
+</style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="home-container">
+        <NavBar :collapsed="collapsed"></NavBar>
+        <div class="home-main" :style="{ marginLeft: (collapsed ? '80px' : '180px') }">
+          <HeaderBar :collapsed.sync="collapsed"></HeaderBar>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import NavBar from './navBar/navBar.vue';
+import HeaderBar from './headerBar/headerBar.vue'
+export default {
+    components: {
+        NavBar,
+        HeaderBar
+        
+    },
+    data() {
+        return {
+            collapsed: false,
+        };
+    },
+  
+};
+</script>
+<style lang="less" scoped>
+.home-container {
+    width: 100%;
+    height: 100%;
+    .home-main {
+        margin-left: 180px;
+        transition: all 300ms;
+
+      
+    }
+}
+</style>
